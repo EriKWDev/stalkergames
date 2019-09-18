93a16055ec23824672bba2bf43c0ef727558fda0
ErikWDev (new Mac) <ErikWDev@gmail.com>
2019-09-18 11:34:16 +0200

    express

--- server.js
+++ server.js
@@ -1,1 +1,12 @@
-console.log("Hello, World")
+var express = require('express');
+var app = express();
+var path = require('path');
+
+// viewed at http://localhost:8080
+app.get('/', function(req, res) {
+    let file = path.join(__dirname, 'static', 'html', 'index.html');
+    res.sendFile(file);
+    console.log(file);
+});
+
+app.listen(80);
